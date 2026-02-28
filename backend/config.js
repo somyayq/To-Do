dec17a6bb1b3b91a6e7868693c4f1eb7c863161b
somya <somyasharma8112004@gmail.com>
2026-02-28 11:05:07 +0530

    Atlas string added

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const  PORT = 5555;
 
-export const MONGO_URI = "mongodb://localhost:27017/ToDo";
+export const MONGO_URI = "mongodb+srv://somya1297be23_db_user:5R3jHrZ8RV96dOBb@cluster0.1hu80fb.mongodb.net/?appName=Cluster0";
