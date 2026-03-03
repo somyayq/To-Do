db09f1df18de1e54ff76121e20e95d0d672dec68
somya <somyasharma8112004@gmail.com>
2026-03-03 17:55:47 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -17,11 +17,7 @@ import { Operation } from "./models/operations.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ["https://to-do-chi-brown.vercel.app/", "http://localhost:5173"],
-  methods: ["GET", "POST", "PATCH", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 // SIGNUP
 app.post("/api/signup", async (req, res) => {
