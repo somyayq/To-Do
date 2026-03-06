41fc862b56369a393dde3c088b7322e84cfb6718
somya <somyasharma8112004@gmail.com>
2026-03-06 19:43:43 +0530

    conages made in MONGO_URI

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 5555;
-export const MONGO_URI = "mongodb://atlas-sql-69a27ac131ee53a2ecc13f1c-hjql3s.a.query.mongodb.net/ToDo?ssl=true&authSource=admin";
+export const MONGO_URI = "mongodb+srv://somya1297be23_db_user:Solan@8112004@cluster0.1hu80fb.mongodb.net/ToDo?retryWrites=true&w=majority&appName=Cluster0";
